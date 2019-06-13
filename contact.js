a10752c99ffd495d4be69e11ce31c98e2983c710
Matthew Bierman <matthew.bierman2@gmail.com>
2019-06-13 02:31:09 -0400

    Added input validation for Request Sample form in Contact page

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-// Submit contact form and error checking
+// Submission and input validation for General Inquiry form
 $("#hib-inquiry-form").submit(function() {
     // Full Name (required)
     if ($("#contact-inquiry-full-name").val() == "") {
@@ -39,7 +39,136 @@ $("#hib-inquiry-form").submit(function() {
     else
         $("#contact-inquiry-message").addClass("is-valid");
     
-    if ($("#contact-inquiry-full-name").val() != "" && $("#contact-inquiry-company").val() != "" && $("#contact-inquiry-email-address").val() != "")
+    // Submit form
+    if ($("#contact-inquiry-full-name").val() != "" &&
+        $("#contact-inquiry-company").val() != "" &&
+        $("#contact-inquiry-email-address").val() != "")
+        return true;
+    else
+        return false;
+});
+
+// Submission and input validation for Sample Request form
+$("#hib-request-form").submit(function() {
+    // Full Name (required)
+    if ($("#contact-request-full-name").val() == "") {
+        $("#contact-request-full-name").removeClass("is-valid");
+        $("#contact-request-full-name").addClass("is-invalid");
+    } else {
+        $("#contact-request-full-name").removeClass("is-invalid");
+        $("#contact-request-full-name").addClass("is-valid");
+    }
+    
+    // Company (required)
+    if ($("#contact-request-company").val() == "") {
+        $("#contact-request-company").removeClass("is-valid");
+        $("#contact-request-company").addClass("is-invalid");
+    } else {
+        $("#contact-request-company").removeClass("is-invalid");
+        $("#contact-request-company").addClass("is-valid");
+    }
+    
+    // Email Address (required)
+    if ($("#contact-request-email-address").val() == "") {
+        $("#contact-request-email-address").removeClass("is-valid");
+        $("#contact-request-email-address").addClass("is-invalid");
+    } else {
+        $("#contact-request-email-address").removeClass("is-invalid");
+        $("#contact-request-email-address").addClass("is-valid");
+    }
+    
+    // Phone Number (optional)
+    if ($("#contact-request-phone-number").val() == "")
+        $("#contact-request-phone-number").removeClass("is-valid");
+    else
+        $("#contact-request-phone-number").addClass("is-valid");
+    
+    // Shipping Address Line 1 (required)
+    if ($("#contact-request-shipping-address").val() == "") {
+        $("#contact-request-shipping-address").removeClass("is-valid");
+        $("#contact-request-shipping-address").addClass("is-invalid");
+    } else {
+        $("#contact-request-shipping-address").removeClass("is-invalid");
+        $("#contact-request-shipping-address").addClass("is-valid");
+    }
+    
+    // Shipping Address Line 2 (optional)
+    if ($("#contact-request-shipping-address-2").val() == "")
+        $("#contact-request-shipping-address-2").removeClass("is-valid");
+    else
+        $("#contact-request-shipping-address-2").addClass("is-valid");
+    
+    // City (required)
+    if ($("#contact-request-city").val() == "") {
+        $("#contact-request-city").removeClass("is-valid");
+        $("#contact-request-city").addClass("is-invalid");
+    } else {
+        $("#contact-request-city").removeClass("is-invalid");
+        $("#contact-request-city").addClass("is-valid");
+    }
+    
+    // State (required)
+    if ($("#contact-request-state").val() == "Choose a State...") {
+        $("#contact-request-state").removeClass("is-valid");
+        $("#contact-request-state").addClass("is-invalid");
+    } else {
+        $("#contact-request-state").removeClass("is-invalid");
+        $("#contact-request-state").addClass("is-valid");
+    }
+    
+    // Zip Code (required)
+    if ($("#contact-request-zip-code").val() == "") {
+        $("#contact-request-zip-code").removeClass("is-valid");
+        $("#contact-request-zip-code").addClass("is-invalid");
+    } else {
+        $("#contact-request-zip-code").removeClass("is-invalid");
+        $("#contact-request-zip-code").addClass("is-valid");
+    }
+    
+    // Country (required)
+    if ($("#contact-request-country").val() == "Choose a Country...") {
+        $("#contact-request-country").removeClass("is-valid");
+        $("#contact-request-country").addClass("is-invalid");
+    } else {
+        $("#contact-request-country").removeClass("is-invalid");
+        $("#contact-request-country").addClass("is-valid");
+    }
+    
+    // Sample Type (required)
+    if ($("#contact-request-sample-type").val() == "Choose a Sample Type...") {
+        $("#contact-request-sample-type").removeClass("is-valid");
+        $("#contact-request-sample-type").addClass("is-invalid");
+    } else {
+        $("#contact-request-sample-type").removeClass("is-invalid");
+        $("#contact-request-sample-type").addClass("is-valid");
+    }
+    
+    // Application of Sample (required)
+    if ($("#contact-request-application-of-sample").val() == "") {
+        $("#contact-request-application-of-sample").removeClass("is-valid");
+        $("#contact-request-application-of-sample").addClass("is-invalid");
+    } else {
+        $("#contact-request-application-of-sample").removeClass("is-invalid");
+        $("#contact-request-application-of-sample").addClass("is-valid");
+    }
+    
+    // Message (optional)
+    if ($("#contact-request-message").val() == "")
+        $("#contact-request-message").removeClass("is-valid");
+    else
+        $("#contact-request-message").addClass("is-valid");
+    
+    // Submit form
+    if ($("#contact-request-full-name").val() != "" &&
+        $("#contact-request-company").val() != "" &&
+        $("#contact-request-email-address").val() != "" &&
+        $("#contact-request-shipping-address").val() != "" &&
+        $("#contact-request-city").val() != "" &&
+        $("#contact-request-state").val() != "Choose a State..." &&
+        $("#contact-request-zip-code").val() != "" &&
+        $("#contact-request-country").val() != "Choose a Country..." &&
+        $("#contact-request-sample-type").val() != "Choose a Sample Type..." &&
+        $("#contact-request-application of sample").val() != "")
         return true;
     else
         return false;
