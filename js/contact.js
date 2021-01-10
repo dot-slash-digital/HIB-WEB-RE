b6ab2459ba87b62a0072bb6009df16375b73f556
Matthew Bierman <matthew.bierman2@gmail.com>
2021-01-10 02:35:03 -0500

    Add changes requested by client

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,9 @@
-// Set contact form submit buttons to disabled on page load
-$(document).ready(function() {
-    $("#hib-contact-submit-inquiry").addClass("disabled");
-    $("#hib-contact-submit-request").addClass("disabled");
-});
-
 // Submission and input validation for General Inquiry form
 $("#hib-inquiry-form").submit(function() {
     var validFN = isFullNameValid($("#contact-inquiry-full-name").val());
     var validPN = isPhoneNumberValid($("#contact-inquiry-phone-number").val());
     var validEA = isEmailAddressValid($("#contact-inquiry-email-address").val());
-    
+
     // Full Name (required)
     if (validFN == false) {
         $("#contact-inquiry-full-name").removeClass("is-valid");
@@ -18,7 +12,7 @@ $("#hib-inquiry-form").submit(function() {
         $("#contact-inquiry-full-name").removeClass("is-invalid");
         $("#contact-inquiry-full-name").addClass("is-valid");
     }
-    
+
     // Company (required)
     if ($("#contact-inquiry-company").val() == "") {
         $("#contact-inquiry-company").removeClass("is-valid");
@@ -27,7 +21,7 @@ $("#hib-inquiry-form").submit(function() {
         $("#contact-inquiry-company").removeClass("is-invalid");
         $("#contact-inquiry-company").addClass("is-valid");
     }
-    
+
     // Email Address (required)
     if (validEA == false) {
         $("#contact-inquiry-email-address").removeClass("is-valid");
@@ -36,7 +30,7 @@ $("#hib-inquiry-form").submit(function() {
         $("#contact-inquiry-email-address").removeClass("is-invalid");
         $("#contact-inquiry-email-address").addClass("is-valid");
     }
-    
+
     // Phone Number (optional)
     if (validPN == "valid") {
         $("#contact-inquiry-phone-number").removeClass("is-invalid");
@@ -46,166 +40,26 @@ $("#hib-inquiry-form").submit(function() {
         $("#contact-inquiry-phone-number").removeClass("is-invalid");
     } else
         $("#contact-inquiry-phone-number").addClass("is-invalid");
-    
-    // Message (optional)
-    if ($("#contact-inquiry-message").val() == "")
+
+    // Message (required)
+    if ($("#contact-inquiry-message").val() == "") {
         $("#contact-inquiry-message").removeClass("is-valid");
-    else
+        $("#contact-inquiry-message").addClass("is-invalid");
+    } else {
         $("#contact-inquiry-message").addClass("is-valid");
-    
+        $("#contact-inquiry-message").removeClass("is-invalid");
+    }
+
     // Submit form
     if (validFN == true &&
         $("#contact-inquiry-company").val() != "" &&
         validEA == true &&
-        (validPN == "valid" || validPN == "empty"))
+        (validPN == "valid" || validPN == "empty") &&
+        $("#contact-inquiry-message").val() != ""
+    ) {
         return true;
-    else {
+    } else {
         showErrorAlert("inquiry");
-        return false;
-    }
-});
-
-// Submission and input validation for Sample Request form
-$("#hib-request-form").submit(function() {
-    var validFN = isFullNameValid($("#contact-request-full-name").val());
-    var validPN = isPhoneNumberValid($("#contact-request-phone-number").val());
-    var validEA = isEmailAddressValid($("#contact-request-email-address").val());
-    var validZC = isZipCodeValid($("#contact-request-zip-code").val());
-    var inUS = ($("#contact-request-country").val() == "Choose a Country..." || $("#contact-request-country").val() == "United States");
-    
-    // Full Name (required)
-    if (validFN == false) {
-        $("#contact-request-full-name").removeClass("is-valid");
-        $("#contact-request-full-name").addClass("is-invalid");
-    } else {
-        $("#contact-request-full-name").removeClass("is-invalid");
-        $("#contact-request-full-name").addClass("is-valid");
-    }
-    
-    // Company (required)
-    if ($("#contact-request-company").val() == "") {
-        $("#contact-request-company").removeClass("is-valid");
-        $("#contact-request-company").addClass("is-invalid");
-    } else {
-        $("#contact-request-company").removeClass("is-invalid");
-        $("#contact-request-company").addClass("is-valid");
-    }
-    
-    // Email Address (required)
-    if (validEA == false) {
-        $("#contact-request-email-address").removeClass("is-valid");
-        $("#contact-request-email-address").addClass("is-invalid");
-    } else {
-        $("#contact-request-email-address").removeClass("is-invalid");
-        $("#contact-request-email-address").addClass("is-valid");
-    }
-    
-    // Phone Number (optional)
-    if (validPN == "valid") {
-        $("#contact-request-phone-number").removeClass("is-invalid");
-        $("#contact-request-phone-number").addClass("is-valid");
-    } else if (validPN == "empty") {
-        $("#contact-request-phone-number").removeClass("is-valid");
-        $("#contact-request-phone-number").removeClass("is-invalid");
-    } else
-        $("#contact-request-phone-number").addClass("is-invalid");
-    
-    // Shipping Address Line 1 (required)
-    if ($("#contact-request-shipping-address").val() == "") {
-        $("#contact-request-shipping-address").removeClass("is-valid");
-        $("#contact-request-shipping-address").addClass("is-invalid");
-    } else {
-        $("#contact-request-shipping-address").removeClass("is-invalid");
-        $("#contact-request-shipping-address").addClass("is-valid");
-    }
-    
-    // Shipping Address Line 2 (optional)
-    if ($("#contact-request-shipping-address-2").val() == "")
-        $("#contact-request-shipping-address-2").removeClass("is-valid");
-    else
-        $("#contact-request-shipping-address-2").addClass("is-valid");
-    
-    // City (required)
-    if ($("#contact-request-city").val() == "") {
-        $("#contact-request-city").removeClass("is-valid");
-        $("#contact-request-city").addClass("is-invalid");
-    } else {
-        $("#contact-request-city").removeClass("is-invalid");
-        $("#contact-request-city").addClass("is-valid");
-    }
-    
-    // State (required)
-    if (inUS == false) {
-        $("#contact-request-state").removeClass("is-valid");
-        $("#contact-request-state").removeClass("is-invalid");
-    } else if ($("#contact-request-state").val() == "Choose a State...") {
-        $("#contact-request-state").removeClass("is-valid");
-        $("#contact-request-state").addClass("is-invalid");
-    } else {
-        $("#contact-request-state").removeClass("is-invalid");
-        $("#contact-request-state").addClass("is-valid");
-    }
-    
-    // Zip Code (required)
-    if (inUS == false) {
-        $("#contact-request-zip-code").removeClass("is-valid");
-        $("#contact-request-zip-code").removeClass("is-invalid");
-    } else if (validZC == false) {
-        $("#contact-request-zip-code").removeClass("is-valid");
-        $("#contact-request-zip-code").addClass("is-invalid");
-    } else {
-        $("#contact-request-zip-code").removeClass("is-invalid");
-        $("#contact-request-zip-code").addClass("is-valid");
-    }
-    
-    // Country (required)
-    if ($("#contact-request-country").val() == "Choose a Country...") {
-        $("#contact-request-country").removeClass("is-valid");
-        $("#contact-request-country").addClass("is-invalid");
-    } else {
-        $("#contact-request-country").removeClass("is-invalid");
-        $("#contact-request-country").addClass("is-valid");
-    }
-    
-    // Sample Type (required)
-    if ($("#contact-request-sample-type").val() == "Choose a Sample Type...") {
-        $("#contact-request-sample-type").removeClass("is-valid");
-        $("#contact-request-sample-type").addClass("is-invalid");
-    } else {
-        $("#contact-request-sample-type").removeClass("is-invalid");
-        $("#contact-request-sample-type").addClass("is-valid");
-    }
-    
-    // Application of Sample (required)
-    if ($("#contact-request-application-of-sample").val() == "") {
-        $("#contact-request-application-of-sample").removeClass("is-valid");
-        $("#contact-request-application-of-sample").addClass("is-invalid");
-    } else {
-        $("#contact-request-application-of-sample").removeClass("is-invalid");
-        $("#contact-request-application-of-sample").addClass("is-valid");
-    }
-    
-    // Message (optional)
-    if ($("#contact-request-message").val() == "")
-        $("#contact-request-message").removeClass("is-valid");
-    else
-        $("#contact-request-message").addClass("is-valid");
-    
-    // Submit form
-    if (validFN == true &&
-        $("#contact-request-company").val() != "" &&
-        validEA == true &&
-        $("#contact-request-shipping-address").val() != "" &&
-        $("#contact-request-city").val() != "" &&
-        (inUS == false || $("#contact-request-state").val() != "Choose a State...") &&
-        (inUS == false || validZC == true) &&
-        $("#contact-request-country").val() != "Choose a Country..." &&
-        $("#contact-request-sample-type").val() != "Choose a Sample Type..." &&
-        $("#contact-request-application-of-sample").val() != "" &&
-        (validPN == "valid" || validPN == "empty"))
-        return true;
-    else {
-        showErrorAlert("request");
         return false;
     }
 });
@@ -220,144 +74,26 @@ $("textarea").on('input', function() {
     $(this).removeClass("is-invalid");
 });
 
-// Display 'Sample Request' form and hide 'General Inquiry' form
-function showRequestForm(scrollTime) {
-    // Inquiry form was visible prior to button click
-    if ($("#toggle-inquiry").hasClass("active")) {
-        // Hide inquiry form, display request form
-        $("#hib-request-form-outer").css("display", "block");
-        $("#hib-inquiry-form-outer").css("display", "none");
-    }
-    
-    // Smooth scroll down to top of form
-    $('html, body').animate({
-        scrollTop: $("#hib-contact-us-buttons").offset().top - $("#hib-header").height() - 5
-    }, scrollTime);
-}
-
-// Display 'General Inquiry' form and hide 'Sample Request' form
-function showInquiryForm(scrollTime) {
-    // Request form was visible prior to button click
-    if ($("#toggle-request").hasClass("active")) {
-        // Hide request form, display inquiry form
-        $("#hib-inquiry-form-outer").css("display", "block");
-        $("#hib-request-form-outer").css("display", "none");
-    }
-    
-    // Smooth scroll down to top of form
-    $('html, body').animate({
-        scrollTop: $("#hib-contact-us-buttons").offset().top - $("#hib-header").height() - 5
-    }, scrollTime);
-}
-
-// Display inquiry form upon clicking toggle button
-$("#toggle-inquiry").click(function(e) {
-    e.preventDefault();
-    showInquiryForm(800);
-});
-
-// Display request form upon clicking toggle button
-$("#toggle-request").click(function(e) {
-    e.preventDefault();
-    showRequestForm(800);
-});
-
-// Display request form if URL parameter has 'sample_request'
-$(function() {
-    var url = new URL(window.location.href);
-    var form_type = url.searchParams.get("form");
-    var product_type = url.searchParams.get("product");
-    
-    if (form_type == "sample_request") {
-        showRequestForm(2000);
-        $("#toggle-inquiry").removeClass("active");
-        $("#toggle-request").addClass("active");
-    }
-    
-    if (product_type == "powder")
-        $("#contact-request-sample-type").val("Organic Hibiscus Extract Powder");
-    else if (product_type == "liquid")
-        $("#contact-request-sample-type").val("Organic Hibiscus Concentrated Liquid Extract");
-});
-
-// Enable submit button when all required fields are valid
-$(".form-control").keyup(function() {
-    // When inquiry form is visible and all required fields are valid
-    if ($("#hib-inquiry-form-outer").css("display") == "block" &&
-        $("#contact-inquiry-full-name").val() != "" &&
-        $("#contact-inquiry-company").val() != "" &&
-        $("#contact-inquiry-email-address").val() != "")
-        $("#hib-contact-submit-inquiry").removeClass("disabled");
-    else
-        $("#hib-contact-submit-inquiry").addClass("disabled");
-    
-    // When request form is visible and all required fields are valid
-    if ($("#hib-request-form-outer").css("display") == "block" &&
-        $("#contact-request-full-name").val() != "" &&
-        $("#contact-request-company").val() != "" &&
-        $("#contact-request-email-address").val() != "" &&
-        $("#contact-request-shipping-address").val() != "" &&
-        $("#contact-request-city").val() != "" &&
-        $("#contact-request-state").val() != "Choose a State..." &&
-        $("#contact-request-zip-code").val() != "" &&
-        $("#contact-request-country").val() != "Choose a Country..." &&
-        $("#contact-request-sample-type").val() != "Choose a Sample Type..." &&
-        $("#contact-request-application-of-sample").val() != "")
-        $("#hib-contact-submit-request").removeClass("disabled");
-    else
-        $("#hib-contact-submit-request").addClass("disabled");
-});
-
 // Display alert error message when form inputs are invalid
 function showErrorAlert(formType) {
     var errorMessage = [];
     var validPN = isPhoneNumberValid($("#contact-" + formType + "-phone-number").val());
     var validFN = isFullNameValid($("#contact-" + formType + "-full-name").val());
     var validEA = isEmailAddressValid($("#contact-" + formType + "-email-address").val());
-    
-    if (formType == "inquiry") {
-        if (validFN == false)
-            errorMessage.push("<span>Enter your full name (first and last name).</span><br />");
-        if ($("#contact-inquiry-company").val() == "")
-            errorMessage.push("<span>Enter your company name.</span><br />");
-        if ($("#contact-inquiry-email-address").val() == "")
-            errorMessage.push("<span>Enter your email address.</span><br />");
-        else if (validEA == false)
-            errorMessage.push("<span>Enter a valid business email address.</span><br />");
-        if (validPN != "valid" && validPN != "empty")
-            errorMessage.push("<span>" + validPN + "</span><br />");
-    } else if (formType == "request") {
-        var validZC = isZipCodeValid($("#contact-request-zip-code").val());
-        var inUS = ($("#contact-request-country").val() == "Choose a Country..." || $("#contact-request-country").val() == "United States");
-        
-        if (validFN == false)
-            errorMessage.push("<span>Enter your full name (first and last name).</span><br />");
-        if ($("#contact-request-company").val() == "")
-            errorMessage.push("<span>Enter your company name.</span><br />");
-        if ($("#contact-request-email-address").val() == "")
-            errorMessage.push("<span>Enter your email address.</span><br />");
-        else if (validEA == false)
-            errorMessage.push("<span>Enter a valid business email address.</span><br />");
-        if (validPN != "valid" && validPN != "empty")
-            errorMessage.push("<span>" + validPN + "</span><br />");
-        if ($("#contact-request-shipping-address").val() == "")
-            errorMessage.push("<span>Enter your shipping address.</span><br />");
-        if ($("#contact-request-city").val() == "")
-            errorMessage.push("<span>Enter your shipping city.</span><br />");
-        if (inUS == true && $("#contact-request-state").val() == "")
-            errorMessage.push("<span>Select your shipping state.</span><br />");
-        if (inUS == true && $("#contact-request-zip-code").val() == "")
-            errorMessage.push("<span>Enter your shipping zip code.</span><br />");
-        else if (inUS == true && validZC == false)
-            errorMessage.push("<span>Enter a valid zip code (e.g. ##### or #####-####).</span><br />");
-        if ($("#contact-request-country").val() == "")
-            errorMessage.push("<span>Select your shipping country.</span><br />");
-        if ($("#contact-request-sample-type").val() == "")
-            errorMessage.push("<span>Select your desired sample type.</span><br />");
-        if ($("#contact-request-application-of-sample").val() == "")
-            errorMessage.push("<span>Enter how you will use the sample.</span><br />");
-    }
-    
+
+    if (validFN == false)
+        errorMessage.push("<span>Enter your full name (first and last name).</span><br />");
+    if ($("#contact-inquiry-company").val() == "")
+        errorMessage.push("<span>Enter your company name.</span><br />");
+    if ($("#contact-inquiry-email-address").val() == "")
+        errorMessage.push("<span>Enter your email address.</span><br />");
+    else if (validEA == false)
+        errorMessage.push("<span>Enter a valid business email address.</span><br />");
+    if (validPN != "valid" && validPN != "empty")
+        errorMessage.push("<span>" + validPN + "</span><br />");
+    if ($("#contact-inquiry-message").val() == "")
+        errorMessage.push("<span>Enter a message.</span><br />");
+
     $("#contact-alert-text").html(errorMessage);
     $("#contact-alert").css("display", "block");
     $("#contact-alert").css("margin-left", "-" + $("#contact-alert").width() / 2 + "px");
@@ -401,7 +137,7 @@ function isEmailAddressValid(emailAddress) {
                 "list",
                 "elhillel"
             ];
-            
+
             for (var i = 0; i < substrings.length; i++) {
                 if (suffix.indexOf(substrings[i]) == 0)
                     return false;
@@ -422,29 +158,6 @@ function isPhoneNumberValid(phoneNumber) {
         else if (phoneNumber.length < 10)
             return "Add a valid area code to your phone number.";
     }
-    
+
     return "valid";
 }
-
-function isZipCodeValid(zipCode) {
-    if ((zipCode.length == 5 && zipCode.replace(/[^0-9]/g, '').length == 5) || (zipCode.length == 10 && zipCode.replace(/[^0-9]/g, '').length == 9 && zipCode.indexOf('-') == 5))
-        return true;
-    else
-        return false;
-}
-
-// disable State and Zip Code input fields if selected country is not United States
-$("#contact-request-country").change(function() {
-    if ($("#contact-request-country").val() != "Choose a Country..." && $("#contact-request-country").val() != "United States") {
-        $("#contact-request-state").prop("disabled", true);
-        $("#contact-request-zip-code").prop("disabled", true);
-        $("#contact-request-state").removeClass("is-valid");
-        $("#contact-request-state").removeClass("is-invalid");
-        $("#contact-request-zip-code").removeClass("is-valid");
-        $("#contact-request-zip-code").removeClass("is-invalid");
-    }
-    else {
-        $("#contact-request-state").prop("disabled", false);
-        $("#contact-request-zip-code").prop("disabled", false);
-    }
-});
